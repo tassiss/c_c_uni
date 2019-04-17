f22faa7d282cae4ec2a4ef4fda7fe5f5f3a16e0c
Thiago Assis da Silva <thiagoassidasilva@ufu.br>
2019-04-17 09:08:07 -0300

    Pós processamento
    Criação de arquivo para instalação de dependências, criação de arquivo python para pós processar já adicionado a um comando make "make gif"

--- bin/out.py
+++ bin/out.py
@@ -7,13 +7,9 @@ l=len(f)
 for i in range (0,l):
 	data=np.loadtxt(f[i])
 	t,x,temp=zip(*data)
-	#print data
-	#print f[i]
 	plt.title('t: '+str(t[0]))
 	plt.xlabel('Posicao do ponto')
 	plt.ylabel('temperatura')
 	plt.plot(x,temp)
-	plt.savefig('../output/'+str(t[0])+'.png')
-	#plt.show()	
+	plt.savefig('../output/'+str(t[0])+'.png')	
 	i=i+1
-	
